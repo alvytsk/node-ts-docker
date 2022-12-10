d9e20b166492fddb7000eb041b1c3a3cd8abc18f
Alexey Vymyatnin <avymiatnin@aspirity.com>
2022-12-10 18:39:54 +0700

    Add express to check if package is working

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,18 @@
+import express from 'express';
 import { analytics } from './utils/analytics.js';
 
 const msg = 'Hello world!!!';
 console.log(msg);
 
 analytics('index.ts');
+
+const app = express();
+const PORT = 3000;
+
+app.get('/', (req, res) => {
+  res.send('Server is up and running!');
+});
+
+app.listen(PORT, () => {
+  console.log(`server started at http://localhost:${PORT}`);
+});
